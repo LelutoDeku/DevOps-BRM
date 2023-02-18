e67155952c31e40a8a60ce32ea81c323d28e1f71
Hammad <hammadsiddiqui1212@gmail.com>
2023-02-18 10:33:37 +0530

    First Maven build

---
+++ Lab 1/Lab_1/src/My_Class.java
@@ -0,0 +1,14 @@
+import pkg_1.*;
+public class My_Class {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        My_Calc ob=new My_Calc();
+        System.out.println(ob.sum(5, 4));
+        System.out.println(ob.diff(5, 4));
+        System.out.println(ob.div(5, 4));
+        System.out.println(ob.mul(5, 4));
+
+    }
+
+}
