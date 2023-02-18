6b64e0f7af8a718a60a93ae24cf5796e4364f6cb
Hammad <hammadsiddiqui1212@gmail.com>
2023-02-18 10:36:29 +0530

    Common maven commands and goals

---
+++ Lab 3/devops.b_r_m.lab_3/src/main/java/pkg1/Class_First.java
@@ -0,0 +1,13 @@
+package pkg1;
+
+public class Class_First {
+	public void show()	{
+		System.out.println("Welcome to Maven");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Class_First c=new Class_First();
+		c.show();
+	}
+
+}
